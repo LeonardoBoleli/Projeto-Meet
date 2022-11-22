914afffcd322841cfbadf12e6bcdbd28bc0fcc62
Leonardo Boleli <leo.boleli@gmail.com>
2022-11-22 18:25:13 -0300

    commit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = require('path');
 server.use(bodyParser.urlencoded({extended:false}));
 server.use(bodyParser.json());
 server.use(express.static("www"));
+server.use(express.static("imagens"));
 server.set('view engine', 'ejs'); /*template ejs */
 server.set('views', './views');
 
