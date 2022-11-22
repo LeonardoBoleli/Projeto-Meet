a34cb3111a570e6b0969f83d5d92942b002a39ae
Leonardo Boleli <leo.boleli@gmail.com>
2022-11-22 18:31:41 -0300

    comi

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 server.use(bodyParser.urlencoded({extended:false}));
 server.use(bodyParser.json());
-//server.use(express.static("www"));
+server.use(express.static("www"));
 //server.use(express.static("imagens"));
 server.use(express.static(path.join(__dirname)));
 server.set('view engine', 'ejs'); /*template ejs */
